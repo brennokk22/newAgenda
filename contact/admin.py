90b7a39a0c01aeddfbe4cf022e776ab065e61119
brennokk22 <brenno.barcellos@live.com>
2023-09-20 21:39:13 -0300

    att

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'first_name', 'last_name', 'phone', 'category'
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
---
+++ runserver.bat
@@ -0,0 +1,2 @@
+chcp 65001 > nul
+cmd /K "".\venv\Scripts\activate.bat" & python manage.py runserver" 
